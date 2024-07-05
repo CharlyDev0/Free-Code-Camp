688eb03d20d675f2eda4623b555b24c249c22f75
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-07-05 11:29:45 -0300

    Step 02 Js

--- JavaScript Algorithms and Data Structures/JavaScript Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures/JavaScript Pyramid Generator/script.js
@@ -1,6 +1,10 @@
+/* Step 01 */
 console.log("Let's build a pyramid using JavaScript basics:");
 console.log("    #    ");
 console.log("   ###   ");
 console.log("  #####  ");
 console.log(" ####### ");
 console.log("#########");
+
+/* Step 02 */
+let character;
