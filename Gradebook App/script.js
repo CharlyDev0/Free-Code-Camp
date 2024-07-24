3f4ae217a4d7aa015f046ab9ae6ef116e93ed755
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-07-24 17:17:17 -0300

    Step 2 Coreection

--- Gradebook App/script.js
+++ Gradebook App/script.js
@@ -11,10 +11,12 @@ function getGrade(score) {
   if (score === 100) {
     return "A++";
   } else if (score > 89) {
+    return "A";}
+    else if (score > 79) {
     return "B";
-  } else if (score > 79) {
-    return "C";
   } else if (score > 69) {
+    return "C";
+  } else if (score > 59) {
     return "D";
   } else{return "F";}
     ;
