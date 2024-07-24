253fc59cadb4f9b71c820753af62ebf987fba895
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-07-24 15:59:20 -0300

    Final Js

--- JavaScript Algorithms and Data Structures/JavaScript Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures/JavaScript Pyramid Generator/script.js
@@ -135,3 +135,57 @@ function padRow(name) {
 /* Step 64 */
 
 function padRow(rowNumber, rowCount) {}
+
+/* Step 65 */
+
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
+/* Step 78 */
+if (condition) {
+  
+}
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
+/* const numbers = [1, 2, 3];
+const shifted = numbers.shift();
+console.log(shifted);
+const unshifted = numbers.unshift(5);
+console.log(unshifted);
+console.log(numbers);*/
+
+
+// ultimo ejercicio
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
