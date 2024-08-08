4fd9fdc3ca2d6265fddf52c25ff38203259883f3
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-08 17:32:23 -0300

    Step 31 Anonin Function Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -9,10 +9,7 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
-/* Step 30
-Finally, use querySelector() to get the #monsterHealth element. Because you have already declared a monsterHealth variable earlier, you need to use a different variable name for this element.
 
-Declare a new variable with the const keyword and name it monsterHealthText.*/
 
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
@@ -21,3 +18,4 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+function goStore() {}
