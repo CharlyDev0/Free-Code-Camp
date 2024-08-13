91d68f3aa16fdcc5f7e6f7291678bcd00a272dcd
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-13 17:36:18 -0300

    Step 59 js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -19,15 +19,26 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-/* Step 59*/
+/* Step 59
+Add a second object to your locations array (remember to separate them with a comma). Following the pattern you used in the first object, create the same properties but use the values from the goStore function. Set the name property to store.*/
 
 const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave , fightDragon],
-    text:"You are in the town square. You see a sign that says \"Store\"."
-  }
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "buttons functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
+  },
 ];
 /* function button (goStore, goCave , fightDragon) */
 
