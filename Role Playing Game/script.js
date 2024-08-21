293129f0d94c07b4a0f6ff7413c21119ab080a3f
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-21 16:51:36 -0300

    Step 82 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -102,8 +102,6 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-/*Step 81
-Just like your locations array, your weapons array will hold objects. Add four objects to the weapons array, each with two properties: name and power. The first should have the name set to "stick" and the power set to 5. The second should be "dagger" and 30. The third, "claw hammer" and 50. The fourth, "sword" and 100.*/
 
 function buyHealth() {
   /* gold = gold - 10;
@@ -118,8 +116,13 @@ function buyHealth() {
  }
 }
 
-function buyWeapon() {
+/* Step 82
+Inside your buyWeapon function, add an if statement to check if gold is greater than or equal to 30. */
 
+function buyWeapon() {
+if (gold>=30) {
+  
+  }
 }
 
 
