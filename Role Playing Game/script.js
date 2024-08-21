39a50e53dd76f1d147f55839f6c16d2f9c1b2bdb
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-21 18:04:41 -0300

    Step 90 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -116,16 +116,23 @@ function buyHealth() {
  }
 }
 
-/* Step 89
-dot notation NAME */
+/* Step 90
+In the previous project, you learned how to work with the concatenation operator to insert variables into a string like this:
+
+Example Code
+const organization = "freeCodeCamp";
+
+// "Hello, our name is freeCodeCamp."
+"Hello, our name is " + organization + ".";
+Update the string "You now have a new weapon." to "You now have a " followed by the name of the new weapon, and remember to end the sentence with a period.*/
 
 function buyWeapon() {
 if (gold>=30) {
   gold -= 30;
   currentWeaponIndex ++;
   goldText.innerText = gold;
-  text.innerText = "You now have a new weapon.";
   let newWeapon = weapons[currentWeaponIndex].name;
+  text.innerText = "You now have a " + newWeapon + ".";
   }
 }
 
