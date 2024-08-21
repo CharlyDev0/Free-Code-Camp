479a9c0c2b784f6217c2002eb80cd90da409a940
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-21 17:30:28 -0300

    Step 85 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -116,15 +116,20 @@ function buyHealth() {
  }
 }
 
-/* Step 84
-The value of the currentWeaponIndex variable corresponds to an index in the weapons array. The player starts with a "stick", since currentWeaponIndex starts at 0 and weapons[0] is the "stick" weapon.
+/* Step 85
+In the previous project, you learned how to use the increment operator to increase a variable by 1.
 
-In the buyWeapon function, use compound assignment to add 1 to currentWeaponIndex - the user is buying the next weapon in the weapons array. */
+Example Code
+let num = 5;
+num++;
+// prints 6
+console.log(num); 
+Change your currentWeaponIndex assignment to use the increment operator. */
 
 function buyWeapon() {
 if (gold>=30) {
   gold -= 30;
-  currentWeaponIndex += 1;
+  currentWeaponIndex ++;
   }
 }
 
