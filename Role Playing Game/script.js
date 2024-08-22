a2e6e6e7818a286dab0c5bdeb2c7093faea81f99
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-22 15:49:07 -0300

    Step 91 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -116,15 +116,10 @@ function buyHealth() {
  }
 }
 
-/* Step 90
-In the previous project, you learned how to work with the concatenation operator to insert variables into a string like this:
+/*Step 91
+Back at the beginning of this project, you created the inventory array. Add the newWeapon to the end of the inventory array using the push() method.
 
-Example Code
-const organization = "freeCodeCamp";
-
-// "Hello, our name is freeCodeCamp."
-"Hello, our name is " + organization + ".";
-Update the string "You now have a new weapon." to "You now have a " followed by the name of the new weapon, and remember to end the sentence with a period.*/
+In the previous project, you learned how to work with the push method like this:*/
 
 function buyWeapon() {
 if (gold>=30) {
@@ -133,6 +128,7 @@ if (gold>=30) {
   goldText.innerText = gold;
   let newWeapon = weapons[currentWeaponIndex].name;
   text.innerText = "You now have a " + newWeapon + ".";
+  inventory.push(newWeapon);
   }
 }
 
