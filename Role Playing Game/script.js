db307234ad2e2b9dfab29cac3bc3022d35869105
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-23 16:10:15 -0300

    Step 107 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -127,23 +127,15 @@ function buyWeapon() {
   }
 }
 
-/* Step 105
-The next step is to create a variable called currentWeapon.
-
-Example Code
-let num = 1;
-if (num === 1) {
-  let num = 2; // this num is scoped to the if statement
-  console.log(num); // expected output: 2
-}
-console.log(num); // expected output: 1 (the global variable)
-Use the let keyword to create a variable named currentWeapon. Don't assign it a value yet.*/
+/* Step 107
+After your currentWeapon, use the concatenation operator to set text.innerText to the string "You sold a ", then currentWeapon, then the string ".".*/
 
 function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
   }
 }
 
