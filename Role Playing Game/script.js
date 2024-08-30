ae77f673e0596025640b37822eb81b15bdc80e69
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-30 17:27:42 -0300

    Step 128 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -181,29 +181,25 @@ function fightDragon() {
   goFight();
 }
 
-/* Step 127
-You can make an else statement conditional by using else if. Here's an example:
-
-Example Code
-if (num > 10) {
-
-} else if (num < 5) {
-
-}
-At the end of your if statement, add an else if statement to check if monsterHealth is less than or equal to 0. In your else if, call the defeatMonster function.*/
+/* Step 128
+At the end of your code, create the defeatMonster and lose functions. Leave them empty for now.*/
 
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks.";
-  text.innerText += " You attack it with your "+ weapons[currentWeaponIndex].name +" .";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeaponIndex].name + " .";
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  monsterHealth -=
+    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
-if (health <= 0) {
-  lose()
-} else if (monsterHealth <=0 ) {
-  defeatMonster();  
-};
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
 
-function dodge(){
-  }};
+  function dodge() {}
+  function defeatMonster() {}
+  function lose() {}
+};
