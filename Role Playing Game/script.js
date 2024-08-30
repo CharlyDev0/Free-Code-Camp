5d8535c15905fc5c99f7fa96b66e90eebfac67b7
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-08-30 17:04:52 -0300

    Step 126

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -108,10 +108,6 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-/* Step 119
-Now, you will need to update the text for the current monster's name and health.
-
-Start by assigning monsters[fighting].name to the innerText property of monsterName. Then, assign monsterHealth to the innerText property of monsterHealthText.*/
 
 function goFight() {
   update(locations[3]);
@@ -185,8 +181,8 @@ function fightDragon() {
   goFight();
 }
 
-/* Step 125
-Update healthText.innerText and monsterHealthText.innerText to equal health and monsterHealth.*/
+/* Step 126
+Add an if statement to check if health is less than or equal to 0. If it is, call the lose function.*/
 
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks.";
@@ -195,6 +191,8 @@ function attack(){
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
+if (health <= 0) {
+  lose()
 };
 
 function dodge() {};
