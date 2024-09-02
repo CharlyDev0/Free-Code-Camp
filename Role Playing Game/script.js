0e2cbb30644c5287c4128857df883e6e6add06be
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-02 17:39:01 -0300

    Step 134 js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -67,12 +67,27 @@ const locations = [
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters.",
   },
- 
+
   {
     name: "fight",
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
+  },
+
+  /* Step 134
+Your locations array doesn't have a fifth element, so locations[4] doesn't work.
+
+Add a new object at the end of the locations array, following the same structure as the other objects. Set name to "kill monster", set "button text" to an array with three "Go to town square" strings, set "button functions" to an array with three goTown variables, and set text to "The monster screams Arg! as it dies. You gain experience points and find gold.".*/
+  {
+    name: "kill monster",
+    "button text": [
+      "Go to town square",
+      "Go to town square",
+      "Go to town square",
+    ],
+    "button functions": [goTown, goTown, goTown],
+    text: "The monster screams Arg! as it dies. You gain experience points and find gold.",
   },
 ];
 
@@ -202,8 +217,6 @@ function attack(){
       "You dodge the attack from the " + monsters[fighting].name + ".";
   }
 
-  /* Step 133
-Finish the defeatMonster function by calling the update function with locations[4] as the argument.*/
 
   function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
