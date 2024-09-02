f57b017904ffb179e5837ddd25d1cef0e1a3e660
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-02 17:57:48 -0300

    Step 137 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -227,5 +227,10 @@ function attack(){
     xpText.innerText = xp;
     update(locations[4]);
   }
-  function lose() {}
+
+  /* Step 137
+In the lose function, call the update function and pass in the sixth object of your locations array. Note that you haven't created this object just yet. */
+  function lose() {
+    update(locations[5]);
+  }
 };
