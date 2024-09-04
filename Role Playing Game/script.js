c16ea9c8a30c04345fb77568cdf2ef9fc0b66b25
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-04 17:06:52 -0300

    Step 144 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -85,8 +85,7 @@ const locations = [
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   },
-  /* Step 143
-Add another object in the locations array. Everything should be the same as the lose object, except the name should be "win" and the text should be "You defeat the dragon! YOU WIN THE GAME! &#x1F389;". */
+  /* */
   {
     name: "lose",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
@@ -207,13 +206,16 @@ function fightDragon() {
   fighting = 2;
   goFight();
 }
+/* Step 144
+While your game is feature-complete at this stage, there are things you can do to make it more fun and engaging. To get started, you'll give monsters a dynamic attack value.
 
+Inside your attack function, change your health -= monsters[fighting].level; line to health -= getMonsterAttackValue(monsters[fighting].level);. This sets health equal to health minus the return value of the getMonsterAttackValue function, and passes the level of the monster as an argument. */
 
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + " .";
-  health -= monsters[fighting].level;
+  health -= getMonsterAttackValue(monsters[fighting].level);
   monsterHealth -=
     weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
