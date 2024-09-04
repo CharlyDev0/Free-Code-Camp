6f79566d706d09a764d4011c3767903b12803cc8
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-04 17:17:55 -0300

    Step 148Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -226,11 +226,11 @@ function attack(){
     }
   }
 }
-/* Step 147
-Log the value of hit to the console to use in debugging. Remember that you can do this with console.log(). */
+/* */
 function getMonsterAttackValue(level){
   const hit = level * 5 - Math.floor(Math.random() * xp);
   console.log(hit);
+  return hit;
 };
   function dodge() {
     text.innerText =
