d6f0bd47246f3b384f23cc7db13c7ea9e340f606
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-14 11:19:37 -0300

    Step 169 Js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -287,7 +287,19 @@ function pickTwo(){pick(2)};
 
 function pickEight(){pick(8)}; 
 
-/* Step 168
+/* Step 169
+The .includes() method determines if an array contains an element and will return either true or false.
+
+Here is an example of the .includes() syntax:
+
+Example Code
+const numbersArray = [1, 2, 3, 4, 5]
+const number = 3
+
+if (numbersArray.includes(number)) {
+  console.log("The number is in the array.")
+}
+After your for loop, add an if statement to check if the guess is in the numbers array. You can use the .includes() method to check if the array contains the guess.
  */
 
 function pick(guess) {
@@ -299,6 +311,9 @@ function pick(guess) {
   for (let i = 0; i < 10; i++) {
    text.innerText += numbers[i] + "\n";
   }
+    if (numbers.includes(guess)) {
+    }
+  
 }
 
   function restart() {
