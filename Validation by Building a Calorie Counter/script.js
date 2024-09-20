49245e66680691f63417e5b6b5da359e0e04838c
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-20 17:04:18 -0300

    Step 15 Js

--- Validation by Building a Calorie Counter/script.js
+++ Validation by Building a Calorie Counter/script.js
@@ -1,1 +1,7 @@
 const calorieCounter = document.getElementById("calorie-counter");
+
+/* Step 15
+Get your #budget element and assign it to budgetNumberInput, and your #entry-dropdown element and assign it to entryDropdown. */
+
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
