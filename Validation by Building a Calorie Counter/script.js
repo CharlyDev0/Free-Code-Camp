7361e5abefe87029d28390572cb50f17b4f0e957
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-25 15:24:04 -0300

    Step 38 Js

--- Validation by Building a Calorie Counter/script.js
+++ Validation by Building a Calorie Counter/script.js
@@ -16,14 +16,12 @@ function cleanInputString(str) {
   const regex = /[+-\s]/g;
   return str.replace(regex, "");
 }
-/* Step 37
-When you open the console, you should see the result of null. The match method returns null when no match is found. In this case, the isInvalidInput function should return null when the input is a valid number without any scientific notation.
-
-null in JavaScript is a special primitive that represents the intentional absence of a value. In a boolean context, null is considered falsy which evaluates to false in a conditional statement.
-
-Now that you have finished testing your isInvalidInput function, you can remove the console.log statement.*/
 
 function isInvalidInput(str){
   const regex = /\d+e\d+/i;
   return str.match(regex);
 };
+/* Step 38
+Your next step is to allow users to add entries to the calorie counter. Declare an empty function addEntry. This function should not take any parameters.*/
+
+function addEntry() {}
