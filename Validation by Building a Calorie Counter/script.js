bc3d2da73409595ccddc035265278a099b73b1d8
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-09-25 16:03:24 -0300

    Step 41 Js

--- Validation by Building a Calorie Counter/script.js
+++ Validation by Building a Calorie Counter/script.js
@@ -21,12 +21,10 @@ function isInvalidInput(str){
   const regex = /\d+e\d+/i;
   return str.match(regex);
 };
-/* Step 40
-Now you need to target the .input-container element within the element that has your targetId. Declare a new targetInputContainer variable, and assign it the value of document.querySelector(). Use concatenation to separate targetId and '.input-container' with a space, and pass that string to querySelector().*/
+/* Step 41
+JavaScript has a feature called template literals, which allow you to interpolate variables directly within a string. Template literals are denoted with backticks ``, as opposed to single or double quotes. Variables can be passed in to a template literal by surrounding the variable with ${} – the value of the variable will be inserted into the string.*/
 
 function addEntry() {
-  const targetId = "#" + entryDropdown.value;
-  let targetInputContainer = document.querySelector(
-    targetId + " .input-container"
-  );
+   const targetId = "#" + entryDropdown.value;
+   const targetInputContainer = document.querySelector(`${targetId} .input-container`);
 }
