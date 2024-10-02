c47f95ad69bcebe92b0937660b8b58de9d0efa61
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-10-02 17:21:38 -0300

    Step 61 Js

--- Validation by Building a Calorie Counter/script.js
+++ Validation by Building a Calorie Counter/script.js
@@ -36,14 +36,14 @@ function addEntry() {
   <input type="number" min="0"  id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />`;
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
   }
-  /* Step 60
-Remember that you wrote a function earlier to clean the user's input? You'll need to use that function here.
-Update your currVal declaration to be the result of calling cleanInputString with item.value.
+  /* Step 61
+You also need to confirm the input is valid. Declare an invalidInputMatch variable, and assign it the result of calling your isInvalidInput function with currVal as the argument.
  */
   function getCaloriesFromInputs (list){
     let calories = 0;
     for (const item of list) {
       const currVal = cleanInputString(item.value);
+      let invalidInputMatch = isInvalidInput(currVal);
     }
   };
   addEntryButton.addEventListener("click", addEntry);
