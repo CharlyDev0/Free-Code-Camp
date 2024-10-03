fbfa162c84b94d97628190af00e456b88bd33a54
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-10-03 17:38:57 -0300

    Step 64 Js

--- Validation by Building a Calorie Counter/script.js
+++ Validation by Building a Calorie Counter/script.js
@@ -36,10 +36,10 @@ function addEntry() {
   <input type="number" min="0"  id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />`;
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
   }
-  /* Step 63
-Browsers have a built in alert() function, which you can use to display a pop-up message to the user. The message to display is passed as the argument to the alert() function.
+  /* Step 64
+In programming, null is meant to represent the absence of a value. In this case, if the user enters an invalid input, you want to alert them and then return null to indicate that the function has failed.
 
-Using a template literal, in your if block, call the alert() function to tell the user "Invalid Input: ", followed by the first value in the invalidInputMatch array.
+Still within your if block, set isError to true and return null.
  */
   function getCaloriesFromInputs (list){
     let calories = 0;
@@ -48,6 +48,8 @@ Using a template literal, in your if block, call the alert() function to tell th
       let invalidInputMatch = isInvalidInput(currVal);
       if (invalidInputMatch) {
         alert(`Invalid Input: ${invalidInputMatch[0]}`);
+        isError = true;
+        return null;
       }
     }
   };
