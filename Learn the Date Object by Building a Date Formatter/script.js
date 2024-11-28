b335104e85426dca891c63229fefe13ae8d4b524
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-11-28 16:56:01 -0300

    023 splip+reverse+join

--- Learn the Date Object by Building a Date Formatter/script.js
+++ Learn the Date Object by Building a Date Formatter/script.js
@@ -8,10 +8,14 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
+const exampleSentence = "selur pmaCedoCeerf".split();
 
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
-      currentDateParagraph.textContent = formattedDate;
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
   }
 });
