9632f814930d6f06ab793d3a54c3fc1adbb2e392
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-02 17:18:48 -0300

    Step 012 Object Freeze

--- Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -3,16 +3,7 @@ const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 
-/* Step 10
-Inside that players array, create a new object with the following properties:
 
-Example Code
-name: "Sergio Almirón"
-position: "forward"
-number: 1
-isCaptain: false
-nickname: null
-NOTE: The numbers for the team are organized alphabetically by last name. This differs from conventional numbering where the numbers correspond with what is on the player's jerseys.*/
 
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
@@ -34,5 +25,20 @@ const myFavoriteFootballTeam = {
       isCaptain: false,
       nickname: null,
     },
+    {
+      name: "Sergio Batista",
+      position: "midfielder",
+      number: 2,
+      isCaptain: false,
+      nickname: null,
+    },
   ],
 };
+/* Step 12
+The rest of the data for the myFavoriteFootballTeam.players array has been filled out for you.
+
+The next step is to ensure that you can't modify this object by adding or removing any properties. We are going to use a method called Object.freeze(obj) which will freeze this object and prevent any changes being made to it.
+
+Use the Object.freeze() method to freeze the myFavoriteFootballTeam object.
+*/
+Object.freeze(myFavoriteFootballTeam);
