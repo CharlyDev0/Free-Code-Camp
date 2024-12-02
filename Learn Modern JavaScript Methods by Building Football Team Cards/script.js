efd8718480d73fd6c66fac25341e3156f0afc7df
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-02 16:08:57 -0300

    Step 003

--- Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -1,5 +1,10 @@
-/* Step 2
-Next, access the id called "sport" from the HTML document and store it in a const variable called typeOfSport. Below that variable, assign the id of "year" to a const variable called worldCupYear.*/
+
 const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
+
+/* Step 3
+Next, access the id called "head-coach" from the HTML document and store it in a const variable called headCoach. Below that variable, assign the id of "player-cards" to a const variable called playerCards..*/
+
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
