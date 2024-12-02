450dcd7cb22d3ba44a7d50e6dcd55c6111cf7266
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-02 15:56:24 -0300

    Step 001

---
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -0,0 +1,9 @@
+/* Step 1
+In this project, you will build a set of football team cards and learn about nested objects, object destructuring, and default parameters. All of the HTML and CSS for this project has been provided for you.
+
+Start by accessing the id called "team" from the HTML document and storing it in a const variable called teamName.
+
+Remember, you can use the getElementById method for this.
+
+NOTE: The numbers for the team are organized alphabetically by last name. This differs from conventional numbering where the numbers correspond with what is on the player's jerseys. */
+const teamName = document.getElementById("team");
