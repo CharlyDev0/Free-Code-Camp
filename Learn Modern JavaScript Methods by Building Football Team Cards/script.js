aa3b753aa20a2cc95c06f154d65fbdd56e69104d
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-02 16:34:54 -0300

    Step 008

--- Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -3,21 +3,19 @@ const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 
-/* Step 7
-Below the team property, add four property key-value pairs.
-
-A key named sport and a string value of "Football".
-A key named year and a number value of 1986.
-A key named isWorldCupWinner and a boolean value set to true.
-A new key called headCoach with a value of an empty object..*/
+/* Step 8
+Inside the headCoach object, add a property with a key of coachName and a string value of "Carlos Bilardo". Below that property, add another key called matches with a number value of 7.*/
 
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
 const myFavoriteFootballTeam = {
-  team:"Argentina"
-  sport:"Football",
-  year:1986,
-  isWorldCupWinner : true,
-  headCoach:{}
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
 };
