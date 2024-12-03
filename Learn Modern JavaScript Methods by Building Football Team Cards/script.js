25ced995bcc9e88474337b302ac80d8e39aea3e7
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-03 17:27:02 -0300

    Step 019

--- Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -37,10 +37,8 @@ const myFavoriteFootballTeam = {
 
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
-/* Step 18
-Now you need to start displaying the team's information on the screen.
 
-Below your destructuring assignments, assign the sport variable to typeOfSport.textContent.
-
-Once you complete that task, you should see the result in the preview window.*/
 typeOfSport.textContent = sport;
+/* Step 19
+Next, assign the team variable to teamName.textContent.*/
+teamName.textContent = team;
