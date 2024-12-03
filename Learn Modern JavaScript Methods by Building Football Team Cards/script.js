a48598561934a5da587b4acc3c984908cc0dcd58
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-03 17:49:38 -0300

    Step 022

--- Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -43,9 +43,16 @@ teamName.textContent = team;
 
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
-/* Step 21
-Now you will start building out the function that will show player cards based on the selections made by the user in the Filter Teammates dropdown menu.
+/* Step 22
+Function parameters can be initialized with default values. If a function is called without an argument, then the default value will be used:
 
-Start by creating an empty arrow function called setPlayerCards. You do not need to add a parameter because that will be taken care of in the next step.*/
+Example Code
+const greeting = (name = "Anonymous") => {
+  return "Hello " + name;
+} 
 
-const setPlayerCards = () => {};
+console.log(greeting("John")); // Hello John
+console.log(greeting()); // Hello Anonymous
+Add a new parameter to your setPlayerCards function called arr and assign it a default value of players. Remember that you destructured the players variable from the myFavoriteFootballTeam object on line 175.*/
+
+const setPlayerCards = (arr = players) => {};
