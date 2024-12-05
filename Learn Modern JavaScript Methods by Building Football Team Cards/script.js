a19a0ea9e270459e695ded963ac9315ca3f38357
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2024-12-05 11:43:17 -0300

    Step 26 Post Bug

--- Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -43,19 +43,9 @@ teamName.textContent = team;
 
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
-/* Step 24
-arr contains a series of objects that each contains a name, position, number, isCaptain and nickname property. In order to access each of those properties inside the callback function, you will need to use object destructuring to unpack them into variables.
-
-Here is an example:
-
-Example Code
-function myExampleFunction({ name, age, job, city }) {
-
-}
-Inside the parameter list in the callback function for the map method, unpack all 5 object properties from objects in arr using object destructuring.*/
-
+/* Step 26*/
 const setPlayerCards = (arr = players) => {playerCards.innerHTML += arr.map(
   ({ name, position, number, isCaptain, nickname }) => {
-    `<div class="player-card"></div>`;
+    `<div class="player-card"><h2>${name}</h2></div>`;    
   }
 );};
